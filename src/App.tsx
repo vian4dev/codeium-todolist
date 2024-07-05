57b01efaab350b8c03ff2977b07b9b9dedde92f0
Gabriel Viana <viannadoze@gmail.com>
2024-07-05 11:25:38 -0300

    Componente de Todo List

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// this component renders a todo list, with task name, completion state and uses the ID as key
+
+type Todo = {
+  id: number
+  name: string
+  completed: boolean
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState<Todo[]>([]) 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Todo List</h1>
+    </div>
+
+    {todoList.map(todo => (
+      <div key={todo.id}>
+        <input type="checkbox" checked={todo.completed} />
+        <span>{todo.name}</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    ))}
     </>
   )
 }
