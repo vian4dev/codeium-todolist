e65cca79a1c542002b3154c13c5be6d9604fc19a
Gabriel Viana <viannadoze@gmail.com>
2024-07-07 13:38:58 -0300

    Refatorando Código

--- src/App.tsx
+++ src/App.tsx
@@ -8,66 +8,51 @@ type Todo = {
 }
 
 function App() {
-  const [todoList, setTodoList] = useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  /**
-   * Adds a new todo item to the todo list.
-   *
-   * @param {React.FormEvent} event - The form event triggered by submitting the form.
-   * @return {void} This function does not return anything.
-   */
-  const addTodo = (event: React.FormEvent) => {
-    event.preventDefault()
-    const target = event.target as typeof event.target & {
-      todo: { value: string }
-    }
+  const handleAddTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const input = event.currentTarget.querySelector('input') as HTMLInputElement;
+    const newTodo: Todo = {
+      id: todos.length,
+      name: input.value,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    input.value = '';
+  };
 
-    const name = target.todo.value
-    setTodoList([...todoList, { id: todoList.length, name, completed: false }])
-    target.todo.value = ''
-  }
+  const handleToggleTodo = (id: number) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+    );
+  };
 
-  /**
-   * Toggles the completed status of a todo item with the given id.
-   *
-   * @param {number} id - The id of the todo item.
-   * @return {void} This function does not return anything.
-   */
-  const toggleCompleted = (id: number) => {
-    setTodoList(todoList.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
-  }
+  const handleRemoveTodo = (todoId: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
 
-  /**
-   * Deletes an item from the todo list based on the provided id.
-   *
-   * @param {number} id - The id of the item to delete
-   */
-  const deleteItem = (id: number) => {
-    setTodoList(todoList.filter(todo => todo.id !== id))
-  }
   return (
-    <>
-      <div className="App">
-        <h1>Todo List</h1>
+    <div className="todo-list">
+      <h1>Todo List</h1>
 
-        <div>
-          <form onSubmit={addTodo}>
-            <input type="text" name="todo" />
-            <button>Add Todo</button>
-          </form>
-        </div>
+      <div>
+        <form onSubmit={handleAddTodo}>
+          <input type="text" name="todo" />
+          <button>Add Todo</button>
+        </form>
       </div>
 
-      {todoList.map(todo => (
-        <div key={todo.id} className="item">
-          <input type="checkbox" checked={todo.completed} onChange={() => {toggleCompleted(todo.id)} }/>
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo-item">
+          <input type="checkbox" checked={todo.completed} onChange={() => handleToggleTodo(todo.id)} />
           <span className={todo.completed ? 'completed' : ''}>{todo.name}</span>
-          <button onClick={() => { deleteItem(todo.id)} }>Delete</button>
+          <button onClick={() => handleRemoveTodo(todo.id)}>Delete</button>
         </div>
-
       ))}
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
